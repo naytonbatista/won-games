c1a5455d15012b1ec7a05c83b728910b449843fc
Nayton Batista <naytonbatista@gmail.com>
2021-12-29 14:32:01 -0400

    Add font

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -34,6 +34,8 @@ const GlobalStyles = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
 
   html {
