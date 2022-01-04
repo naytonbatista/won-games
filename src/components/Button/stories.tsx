985a0e779eb23b0edd3dfc273395457691c58bca
Nayton Batista <naytonbatista@gmail.com>
2022-01-04 18:42:38 -0400

    Add Button Component

---
+++ src/components/Button/stories.tsx
@@ -0,0 +1,33 @@
+import { Story, Meta } from '@storybook/react'
+import Button from '.'
+
+import { AddShoppingCart } from '@styled-icons/material-outlined/AddShoppingCart'
+
+export default {
+  title: 'Button',
+  component: Button,
+  argTypes: {
+    children: {
+      type: 'string'
+    },
+    icon: {
+      control: {
+        type: null
+      }
+    }
+  }
+} as Meta
+
+export const Default: Story = (args) => <Button {...args} />
+
+Default.args = {
+  children: 'Buy Now'
+}
+
+export const withIcon: Story = (args) => <Button {...args} />
+
+withIcon.args = {
+  size: 'small',
+  children: 'Buy Now',
+  icon: <AddShoppingCart />
+}
