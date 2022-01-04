43ff90a5aeec31ce1c0d298905341e5e33b9fbea
Nayton Batista <naytonbatista@gmail.com>
2022-01-04 16:37:55 -0400

    Add MediaMatch Component

---
+++ src/components/MediaMatch/stories.tsx
@@ -0,0 +1,21 @@
+import { Story, Meta } from '@storybook/react'
+import MediaMatch from '.'
+
+export default {
+  title: 'MediaMatch',
+  component: MediaMatch
+} as Meta
+
+export const Desktop: Story = () => (
+  <MediaMatch greaterThan="medium">Only Desktop</MediaMatch>
+)
+
+export const Mobile: Story = () => (
+  <MediaMatch lessThan="medium">Only Mobile</MediaMatch>
+)
+
+Mobile.parameters = {
+  viewport: {
+    defaultViewport: 'mobile1'
+  }
+}
