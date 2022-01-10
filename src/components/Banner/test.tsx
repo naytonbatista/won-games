1383c36a8101fb5d75172fbc7206beebb2c29190
Nayton Batista <naytonbatista@gmail.com>
2022-01-09 20:20:58 -0400

    Adjust Banner to beahve with Ribbon

--- src/components/Banner/test.tsx
+++ src/components/Banner/test.tsx
@@ -25,4 +25,25 @@ describe('<Banner />', () => {
     expect(screen.getByRole('img')).toHaveAttribute('src', '/images/foo.png')
     expect(container.firstChild).toMatchSnapshot()
   })
+
+  it('should render a Ribbon', () => {
+    renderWithTheme(
+      <Banner
+        img="/images/foo.png"
+        title="Defy death"
+        subtitle="Play the new CrasLands season"
+        buttonLabel="Buy now"
+        buttonLink="/games"
+        ribbon="My Ribbon"
+        ribbonSize="small"
+        ribbonColor="secondary"
+      />
+    )
+
+    const ribbon = screen.getByText(/my ribbon/i)
+
+    expect(ribbon).toBeInTheDocument()
+    expect(ribbon).toHaveStyle({ backgroundColor: '#3CD3C1' })
+    expect(ribbon).toHaveStyle({ height: '2.6rem', fontSize: '1.2rem' })
+  })
 })
