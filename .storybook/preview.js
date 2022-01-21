4658a37fc00ac56599b2e54ca817d402775a2ad7
Nayton Batista <naytonbatista@gmail.com>
2022-01-21 02:51:56 -0400

    Change storybook backgrounds config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -4,6 +4,19 @@ import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
 
 export const parameters = {
+  backgrounds: {
+    default: 'won-light',
+    values: [
+      {
+        name: 'won-light',
+        value: theme.colors.white
+      },
+      {
+        name: 'won-dark',
+        value: theme.colors.mainBg
+      }
+    ]
+  },
   nextRouter: {
     Provider: RouterContext.Provider
   }
