a98b226bb5c987e6884594200b5b3c440c0646cd
Nayton Batista <naytonbatista@gmail.com>
2022-05-02 13:40:39 -0400

    Add Textfield component

--- src/components/TextField/stories.tsx
+++ src/components/TextField/stories.tsx
@@ -1,9 +1,24 @@
 import { Story, Meta } from '@storybook/react'
-import TextField from '.'
+
+import TextField, { TextFieldProps } from '.'
 
 export default {
   title: 'TextField',
-  component: TextField
+  component: TextField,
+  args: {
+    label: 'E-mail',
+    labelFor: 'Email',
+    id: 'Email',
+    initialValue: '',
+    placeholder: 'naytonbatista@yahoo.com.br'
+  },
+  argtypes: {
+    onInput: { action: 'changed' }
+  }
 } as Meta
 
-export const Default: Story = () => <TextField />
+export const Default: Story<TextFieldProps> = (args) => (
+  <div style={{ maxWidth: 300, padding: 15 }}>
+    <TextField {...args} />
+  </div>
+)
--- src/components/TextField/test.tsx
+++ src/components/TextField/test.tsx
@@ -1,15 +1,61 @@
-import { render, screen } from '@testing-library/react'
-
+import { screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { Email } from '@styled-icons/material-outlined'
 import TextField from '.'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 describe('<TextField />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<TextField />)
+  it('should render with Label', () => {
+    renderWithTheme(<TextField label="Label" name="Label" />)
 
-    expect(
-      screen.getByRole('heading', { name: /TextField/i })
-    ).toBeInTheDocument()
+    expect(screen.getByLabelText('Label')).toBeInTheDocument()
+  })
 
-    expect(container.firstChild).toMatchSnapshot()
+  it('should render without Label', () => {
+    renderWithTheme(<TextField />)
+
+    expect(screen.queryByLabelText('Label')).not.toBeInTheDocument()
+  })
+
+  it('should render with placeholder', () => {
+    renderWithTheme(<TextField placeholder="Digite aqui" />)
+
+    expect(screen.getByPlaceholderText('Digite aqui')).toBeInTheDocument()
+  })
+
+  it('should render with icon', () => {
+    renderWithTheme(<TextField icon={<Email data-testid="icon" />} />)
+
+    expect(screen.getByTestId('icon')).toBeInTheDocument()
+  })
+
+  it('should render with icon on the right side', () => {
+    renderWithTheme(
+      <TextField icon={<Email data-testid="icon" />} iconPosition="right" />
+    )
+
+    expect(screen.getByTestId('icon').parentElement).toHaveStyle({ order: 1 })
+  })
+
+  it('should changes its value when typing', async () => {
+    const onInputChange = jest.fn()
+
+    renderWithTheme(
+      <TextField
+        onInputChange={onInputChange}
+        label="textField"
+        name="textField"
+      />
+    )
+
+    const input = screen.getByRole('textbox')
+    const text = 'This is my new text'
+    userEvent.type(input, text)
+
+    await waitFor(() => {
+      expect(input).toHaveValue(text)
+      expect(onInputChange).toHaveBeenCalledTimes(text.length)
+    })
+    expect(onInputChange).toHaveBeenCalledWith(text)
   })
 })
