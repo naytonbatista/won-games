a63b7d455a3b556d7d98a05f1483c846c3fe3c0a
Nayton Batista <naytonbatista@gmail.com>
2022-12-08 17:36:16 -0400

    add link to home page

--- src/templates/Auth/index.tsx
+++ src/templates/Auth/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import Heading from 'components/Heading'
 import Logo from 'components/Logo'
 import * as S from './styles'
@@ -11,7 +12,12 @@ const Auth = ({ title, children }: AuthProps) => (
   <S.Wrapper>
     <S.BannerBlock>
       <S.BannerContent>
-        <Logo id="banner" />
+        <Link href="/">
+          <a>
+            {' '}
+            <Logo id="banner" />
+          </a>
+        </Link>
         <div>
           <Heading size="huge">All your favorite games in one place</Heading>
           <S.Subtitle>
@@ -24,7 +30,12 @@ const Auth = ({ title, children }: AuthProps) => (
     </S.BannerBlock>
     <S.Content>
       <S.ContentWrapper>
-        <Logo color="black" size="large" id="content" />
+        <Link href="/">
+          <a>
+            <Logo color="black" size="large" id="content" />
+          </a>
+        </Link>
+
         <Heading color="black" lineColor="secondary" lineLeft>
           {title}
         </Heading>
--- src/templates/Auth/styles.ts
+++ src/templates/Auth/styles.ts
@@ -47,6 +47,11 @@ export const BannerContent = styled.div`
     position: relative;
     z-index: ${theme.layers.base};
     color: ${theme.colors.white};
+
+    a {
+      width: fit-content;
+      height: fit-content;
+    }
   `}
 `
 export const Subtitle = styled.h3`
