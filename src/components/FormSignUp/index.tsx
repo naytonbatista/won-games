99ec5f530fa7d8b4da854a3b7796d00e896f707f
Nayton Batista <naytonbatista@gmail.com>
2022-12-08 15:23:04 -0400

    Add FormSignUp Component

---
+++ src/components/FormSignUp/index.tsx
@@ -0,0 +1,48 @@
+import { Email, Lock, AccountCircle } from '@styled-icons/material-outlined'
+import Button from 'components/Button'
+import TextField from 'components/TextField'
+import Link from 'next/link'
+import { FormWrapper, FormLink } from 'components/Form'
+
+const FormSignIn = () => (
+  <FormWrapper>
+    <form>
+      <TextField
+        name="name"
+        placeholder="Name"
+        type="text"
+        icon={<AccountCircle />}
+      />
+      <TextField
+        name="email"
+        placeholder="Email"
+        type="email"
+        icon={<Email />}
+      />
+      <TextField
+        name="password"
+        placeholder="Password"
+        type="password"
+        icon={<Lock />}
+      />
+      <TextField
+        name="confirm-password"
+        placeholder="Confirm password"
+        type="password"
+        icon={<Lock />}
+      />
+
+      <Button size="large" fullWidth>
+        Sign up now
+      </Button>
+      <FormLink>
+        Already have an account?{' '}
+        <Link href="/sign-in">
+          <a>Sign in</a>
+        </Link>
+      </FormLink>
+    </form>
+  </FormWrapper>
+)
+
+export default FormSignIn
