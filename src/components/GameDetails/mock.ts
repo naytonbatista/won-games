1a1f70ff41ee7a7ad2a79c6de4b9d89a14017c55
Nayton Batista <naytonbatista@gmail.com>
2022-12-13 18:03:49 -0400

    add GameDetails mock

---
+++ src/components/GameDetails/mock.ts
@@ -0,0 +1,8 @@
+export default {
+  developer: 'Diferent tales',
+  releaseDate: '2020-11-21T23:00:00',
+  platforms: ['windows', 'mac', 'linux'],
+  publisher: 'Walkabout',
+  rating: 'BR0',
+  genres: ['Role-playng']
+}
