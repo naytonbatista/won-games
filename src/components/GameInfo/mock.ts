783d628b84f49a21bd0d4bb3895df9adc6e62b96
Nayton Batista <naytonbatista@gmail.com>
2022-12-13 14:47:39 -0400

    GameInfo Component

---
+++ src/components/GameInfo/mock.ts
@@ -0,0 +1,6 @@
+export default {
+  title: 'Boderland 3',
+  price: '215,00',
+  description:
+    'Experience the epic space strategy games that redefined the rts genre. Control your fleet and build an armada across more than 30 single-player missions.'
+}
