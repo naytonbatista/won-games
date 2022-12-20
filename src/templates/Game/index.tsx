4557a7abb1a710f5cbd6833dbe739fc2d0bf992c
Nayton Batista <naytonbatista@gmail.com>
2022-12-20 16:44:34 -0400

    Add Empty component

--- src/templates/Game/index.tsx
+++ src/templates/Game/index.tsx
@@ -1,3 +1,4 @@
+import { Divider } from 'components/Divider'
 import Gallery, { GalleryImageProps } from 'components/Gallery'
 import { GameCardProps } from 'components/GameCard'
 import GameDetails, { GameDetailsProps } from 'components/GameDetails'
@@ -43,6 +44,7 @@ const Game = ({
       </S.SectionDescription>
       <S.SectionGameDetails>
         <GameDetails {...details} />
+        <Divider />
       </S.SectionGameDetails>
       <ShowCase
         title="upcoming"
