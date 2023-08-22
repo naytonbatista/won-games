f97043b6fc6e23ab69c84bd1d071ea4f99bc248b
Nayton Batista <naytonbatista@gmail.com>
2023-08-22 14:48:45 -0400

    Add PaymentOptions Component

---
+++ src/components/PaymentOptions/index.tsx
@@ -0,0 +1,67 @@
+import { Add, ShoppingCart } from '@styled-icons/material-outlined'
+import Button from 'components/Button'
+import Heading from 'components/Heading'
+import Radio from 'components/Radio'
+import Image from 'next/image'
+import * as S from './styles'
+import { useState } from 'react'
+
+export type PaymentCard = {
+  number: string
+  flag: string
+  img: string
+}
+
+export type PaymentOptionsProps = {
+  cards: PaymentCard[]
+  handlePayment: () => void
+}
+
+const PaymentOptions = ({ cards, handlePayment }: PaymentOptionsProps) => {
+  const [checked, setChecked] = useState(false)
+  return (
+    <S.Wrapper>
+      <S.Body>
+        <Heading color="black" size="small" lineBottom>
+          Payment
+        </Heading>
+        <S.CardList>
+          {cards?.map((card) => (
+            <S.CardItem key={card.number}>
+              <S.CardInfo>
+                <Image src={card.img} alt={card.flag} width={38} height={24} />
+                {card.number}
+              </S.CardInfo>
+              <Radio
+                name="credit-card"
+                id={card.number}
+                value={card.number}
+                onCheck={() => {
+                  setChecked(true)
+                }}
+              />
+            </S.CardItem>
+          ))}
+          <S.AddCard role="button">
+            <Add size={14} /> Add a new credit card
+          </S.AddCard>
+        </S.CardList>
+      </S.Body>
+      <S.Footer>
+        <Button as="a" fullWidth minimal>
+          Continue shopping
+        </Button>
+        <Button
+          fullWidth
+          icon={<ShoppingCart />}
+          onClick={handlePayment}
+          disabled={!checked}
+        >
+          Buy now
+        </Button>
+      </S.Footer>
+    </S.Wrapper>
+  )
+}
+
+export default PaymentOptions
